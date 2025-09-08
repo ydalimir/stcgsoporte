86a00f6f7afd88df477cf96c1ab3c4e190d894c1
Edgar Arevalo <sistemas@garbho.mx>
2025-09-08 13:18:26 +0000

    nose mira la iamgen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
         <div className="container mx-auto px-4 grid md:grid-cols-2 gap-12 items-center">
           <div className="relative h-80 md:h-[500px] rounded-lg overflow-hidden shadow-2xl order-last md:order-first">
             <Image
-              src="https://picsum.photos/600/800"
+              src="https://www.sticgsa.com/assets/img/aaaIMG_6564.jpg"
               alt="Equipo de sticgsa"
               data-ai-hint="team workshop"
               fill
