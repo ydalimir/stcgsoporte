233e734efc50dd0bc1fb90d72320d67bc481a84f
Edgar Arevalo <sistemas@garbho.mx>
2026-01-13 06:31:20 +0000

    RECUEDA UE LAS CRECNIALES PRINCIPÁLES SE LALAMDANDE DESD E EL .env.local

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,17 @@
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import 'dotenv/config';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmHehK8ifXw4Syz3PIQrZMFXapGqHgOhM",
-  authDomain: "stcigsa.firebaseapp.com",
-  projectId: "stcigsa",
-  storageBucket: "stcigsa.appspot.com",
-  messagingSenderId: "934441754028",
-  appId: "1:934441754028:web:664e0283ffbe20ee2a2e09",
-  measurementId: "G-P2LQXQYTCX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
